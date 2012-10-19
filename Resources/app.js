9a370896d5495e434c341bfbf1764b968260a1e0
Srdjan Lukic-Bardak <srdjan@lukic.nu>
2012-10-19 17:09:21 +0200

    It starts to look like something...

--- Resources/app.js
+++ Resources/app.js
@@ -1,4 +1,4 @@
-Titanium.UI.setBackgroundColor('#333');
+Titanium.UI.setBackgroundImage('iphone/Default.png');
 
 (function() {
 		
@@ -6,11 +6,84 @@ Titanium.UI.setBackgroundColor('#333');
 		
 	
 	var win = Ti.UI.createWindow({
-		backgroundColor:'#333'
+		layout:'vertical'
+	});
+	
+	var headline = Ti.UI.createLabel({
+		text:'Radio buttons',
+		color:'#fff',
+		font:{fontSize:23,fontWeight:'Bold'},
+		shadowColor:'#000',
+		shadowOffset:{x:1,y:1},
+		top:0,
+		textAlign:'center',
+		height:45
+	});
+	
+	var text = Ti.UI.createLabel({
+		text:'Basic JS model that might be handy for someone out there. \nWorks on iOS and Android',
+		color:'#fff',
+		font:{fontSize:15,fontWeight:'Bold'},
+		shadowColor:'#000',
+		shadowOffset:{x:1,y:1},
+		textAlign:'center',
+		top:10
+	});
+	
+	var headline2 = Ti.UI.createLabel({
+		text:'Layout: horizontal',
+		color:'#fff',
+		font:{fontSize:23,fontWeight:'Bold'},
+		shadowColor:'#000',
+		shadowOffset:{x:1,y:1},
+		top:0,
+		textAlign:'center',
+		height:45
+	});	
+	
+	var radioGroup = radioButton.createGroup({
+		groupId:1,
+		width:119,
+		height:34,
+		layout:'horizontal',
+		radioItemsValue:['One', 'Two', 'Three'],
+		radioItemsPadding:10,
+		radioItemsBackgroundSelectedImage:'/iphone/radioButtonActive.png',
+		radioItemsBackgroundImage:'/iphone/radioButton.png',
+		radioItemsWidth:33,
+		radioItemsHeight:34
 	});
 	
 	
-
+	var headline3 = Ti.UI.createLabel({
+		text:'Layout: vertical',
+		color:'#fff',
+		font:{fontSize:23,fontWeight:'Bold'},
+		shadowColor:'#000',
+		shadowOffset:{x:1,y:1},
+		top:0,
+		textAlign:'center',
+		height:45
+	});		
+	var radioGroup2 = radioButton.createGroup({
+		groupId:1,
+		width:34,
+		height:150,
+		layout:'vertical',
+		radioItemsValue:['One', 'Two', 'Three'],
+		radioItemsPadding:10,
+		radioItemsBackgroundSelectedImage:'/iphone/radioButtonActive.png',
+		radioItemsBackgroundImage:'/iphone/radioButton.png',
+		radioItemsWidth:33,
+		radioItemsHeight:34
+	});	
+	
+	win.add(headline);
+	win.add(text);
+	win.add(headline2);
+	win.add(radioGroup);
+	win.add(headline3);
+	win.add(radioGroup2);
 	win.open();
 
 })();
--- Resources/ui/tiRadioButton.js
+++ Resources/ui/tiRadioButton.js
@@ -1,5 +1,56 @@
 exports.createGroup = function(arg) {
 	
+	//// Main object that will be returned when createGroup is called.
+	var self = Ti.UI.createView({
+		width:arg.width,
+		height:arg.height,
+		groupId: arg.groupId,
+		layout:'horizontal',
+		top:20
+	});
+
+	//// Looping through radio buttons that will be created inside the radioGroup. 
+	for(var i = 0, count = arg.radioItemsValue.length; i<count; i++){
+		var radioItem = Ti.UI.createImageView({
+			width:arg.radioItemsWidth,
+			height:arg.radioItemsHeight,
+			image:arg.radioItemsBackgroundImage,
+			top:0,
+			left:0,
+			id:i
+		});	
 	
-	var self = Ti.UI.createView();
+		//// Adding left padding/margin/spacing radio items. Ofc. not the first one ;) 
+		if(i!=0 && arg.layout ==='horizontal')
+		{
+			radioItem.setLeft(arg.radioItemsPadding);
+		}
+		if(i!=0 && arg.layout ==='vertical')
+		{
+			radioItem.setTop(arg.radioItemsPadding);
+		}
+		
+		//// Adding singletap event that will chnage background image on the selected radio item.
+		radioItem.addEventListener('singletap', function(e) {
+    		var _parent = this.getParent();
+    	
+    		for(var i = 0, count = _parent.children.length; i<count; i++){
+    			if(i === e.source.id)
+    			{
+    				_parent.children[i].setImage(arg.radioItemsBackgroundSelectedImage);
+    			}
+    			else{
+    				_parent.children[i].setImage(arg.radioItemsBackgroundImage);	
+    			}
+			}		
+		});
+	
+		//// Adding radio button to our main object/container/holder
+		self.add(radioItem);
+	}
+	
+	
+
+	//// Returning the main object that contains radio buttons. 
+	return self;
 };
