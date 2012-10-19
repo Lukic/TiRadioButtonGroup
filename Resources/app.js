201661325a67158dc77a4ac4502c2297d21ba7f4
Srdjan Lukic-Bardak <srdjan@lukic.nu>
2012-10-19 13:10:26 +0200

    Cleaning up

--- Resources/app.js
+++ Resources/app.js
@@ -1,16 +1,1 @@
 Titanium.UI.setBackgroundColor('#000');
-
-var offCanvas = {};
-
-(function() {
-		var offCanvas	= require('ui/windowHandler'),
-		windowMain		= require('windows/Main'),
-		windowNav		= require('windows/Navigation');
-		
-		
-		
-		offCanvas.create(windowNav(),windowMain()).open();
-
-
-
-})();
